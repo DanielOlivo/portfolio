50a01a1138f24014f976794b2eebf005b47d0c2f
DanielOlivo <vit.masterov@gmail.com>
2025-01-29 14:01:15 +0200

    nothing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    // emptyOutDir: true,
+    // lib: {
+    //   entry: 'src/main.tsx',
+    //   formats: ['cjs', 'es']
+    // }
+  },
   plugins: [
+    
     react(),
   ],
 })
